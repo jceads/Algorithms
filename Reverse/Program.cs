1e54b0436cd27818a73521196cb355564c1f4564
jcead <jceadsa@gmail.com>
2023-07-27 14:55:26 +0300

    reverse added

---
+++ Reverse/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] arr = {9,8,7,6,5,4,3,2,1};
+      
+Console.WriteLine("Reversed");
+Reverse(arr).ToList().ForEach(Console.WriteLine);
+
+int[] Reverse(int[]arr){
+    int len = arr.Length - 1;
+    for(int i=0;i<(int)Math.Floor((float)arr.Length/2);i++){
+        (arr[i],arr[len-i])=(arr[len-i],arr[i]);
+    }
+      
+    return arr;
+}
